9b45384c27aab13c67ced35e3cd80052fb7f52aa
Emil Stenqvist <emsten@gmail.com>
2015-03-08 19:43:48 +0100

    Fix indentation

--- events/events.js
+++ events/events.js
@@ -4,27 +4,26 @@ var events = {
         var canJump = true;
         var canWalkIntoDoor = true;
 
-    	function movePlayer (e, isKeyDown) {
+        function movePlayer (e, isKeyDown) {
 
-    		if(e.keyCode === actions.PLAYER_DOWN || e.keyCode === actions.ARROW_KEY_DOWN){
-				player.setGoDown(isKeyDown);        		
-        	}
+            if(e.keyCode === actions.PLAYER_DOWN || e.keyCode === actions.ARROW_KEY_DOWN){
+                player.setGoDown(isKeyDown);
+            }
 
-        	if(e.keyCode === actions.PLAYER_UP || e.keyCode === actions.ARROW_KEY_UP){
-				player.setGoUp(isKeyDown);        		
+            if(e.keyCode === actions.PLAYER_UP || e.keyCode === actions.ARROW_KEY_UP){
+                player.setGoUp(isKeyDown);
 
-        	}
+            }
 
-        	if(e.keyCode === actions.PLAYER_LEFT || e.keyCode === actions.ARROW_KEY_LEFT){
-				player.setGoLeft(isKeyDown);        		
-        	}
+            if(e.keyCode === actions.PLAYER_LEFT || e.keyCode === actions.ARROW_KEY_LEFT){
+                player.setGoLeft(isKeyDown);
+            }
 
-        	if(e.keyCode === actions.PLAYER_RIGHT || e.keyCode === actions.ARROW_KEY_RIGHT){
-				player.setGoRight(isKeyDown);        		
-        	}
+            if(e.keyCode === actions.PLAYER_RIGHT || e.keyCode === actions.ARROW_KEY_RIGHT){
+                player.setGoRight(isKeyDown);
+            }
 
             if(e.keyCode === actions.ARROW_KEY_UP && isKeyDown){
-                
                 player.toggleGravity();
             }
 
@@ -40,31 +39,31 @@ var events = {
                 }
             }
 
-        	if(e.keyCode === actions.PLAYER_JUMP) {
+            if(e.keyCode === actions.PLAYER_JUMP) {
 
                 if(isKeyDown && canJump) {
-        		  player.jump(isKeyDown);
+                    player.jump(isKeyDown);
 
-                  canJump = false;
+                    canJump = false;
                 }
 
                 if(!isKeyDown){ //Key up
                     canJump = true;
-                }   
+                }
 
-        	}
+            }
 
             e.preventDefault();
         }
 
-    	var actions = keyEnum;
+        var actions = keyEnum;
 
         document.body.addEventListener("keydown", function(e){
-        	movePlayer(e, true);	
+            movePlayer(e, true);
         });
 
         document.body.addEventListener("keyup", function(e) {
-        	movePlayer(e, false);
+            movePlayer(e, false);
         });
     }
 };
